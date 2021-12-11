e78f756e39054f2556c6e00a94e2145b5a4aaaff
Zeluck <85510521+Zeluck-tv@users.noreply.github.com>
2021-12-11 12:21:09 +0100

    Add files via upload

---
+++ name/__resource.lua
@@ -0,0 +1,11 @@
+description 'FiveM script for vehicles that are in "null" in your garage or when you lock it ect ...'
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+-- Dev by Zeluck
